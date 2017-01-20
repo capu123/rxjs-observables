ac367cb1603662a2465c39eae51b0a8e7361ca74
Saideep Chhetri <saideep_chhetri@yahoo.com>
2017-01-20 15:20:09 +0000

    Observables from Events

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+// Observables from Events
+const btn = $('#btn');
+
+const btnStream$ = Rx.Observable.fromEvent(btn, 'click');
+
+btnStream$.subscribe(
+    function (e) {
+        console.log(e.target.innerHTML);
+    },
+    function (err) {
+        console.log(err);
+    },
+    function () {
+        console.log('Completed');
+    }
+)
